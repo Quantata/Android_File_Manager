d871ab4759db8b429c60844d2824bba2eb10207f
Quantata <ceoahn95@gmail.com>
2021-01-04 19:18:47 +0900

    initial commit

---
+++ app/src/main/java/com/example/nyahn_fileexplorer/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.nyahn_fileexplorer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        init();
+
+    }
+
+    public void init() {
+        // toolbar as actionbar
+        // setting toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.title_main_page);
+        setSupportActionBar(toolbar);
+
+
+
+
+    }
+}
