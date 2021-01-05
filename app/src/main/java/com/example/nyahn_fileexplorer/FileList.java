68269e6eb3d82f1330c41fe46ca59aaca579fe0e
Quantata <ceoahn95@gmail.com>
2021-01-05 09:55:51 +0900

    FileLIst 주석 달음

--- app/src/main/java/com/example/nyahn_fileexplorer/FileList.java
+++ app/src/main/java/com/example/nyahn_fileexplorer/FileList.java
@@ -1,6 +1,7 @@
 package com.example.nyahn_fileexplorer;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -17,6 +18,7 @@ public class FileList extends ListActivity
 {
     private File file;
     private List<String> fileList;
+    private final String rootMainDir = Environment.getExternalStorageDirectory().toString();
 
     public void onCreate(Bundle savedInstanceState)
     {
@@ -24,11 +26,12 @@ public class FileList extends ListActivity
 
         fileList = new ArrayList<>();
 
-        // sdcard
-        String rootInnerDir = Environment.getExternalStorageDirectory().toString();
-        Log.i("FileList : ", rootInnerDir);
+        // External Storage
+//        rootMainDir = Environment.getExternalStorageDirectory().toString();
+        Log.i("FileList : ", rootMainDir);
 
-        file = new File(rootInnerDir) ;
+        // rootMainDir에 해당되는 파일의 File 객체 생성
+        file = new File(rootMainDir) ;
         File[] list = file.listFiles();
 
         for (File value : list) {
@@ -44,12 +47,15 @@ public class FileList extends ListActivity
     {
         super.onListItemClick(l, v, position, id);
 
-        File tempFile = new File(file, fileList.get( position ));
+        // file 객체 폴더의 fileList.get(position)라는 파일에 대한 파일 객체를 생성
+        File clickedFile = new File(file, fileList.get( position ));
 
-        Log.i("FileList : ", ""+ tempFile.isFile());
-        if(!tempFile.isFile())
+        Log.i("FileList : ", ""+ clickedFile.isFile());
+
+        if(!clickedFile.isFile())
         {
-            file = new File( file, fileList.get( position ));
+//            file = new File( file, fileList.get( position ));
+            file = clickedFile;
             File[] list = file.listFiles();
 
             fileList.clear();
@@ -57,7 +63,7 @@ public class FileList extends ListActivity
             for (File value : list) {
                 fileList.add(value.getName());
             }
-            Toast.makeText(getApplicationContext(), file.toString(), Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), file.toString(), Toast.LENGTH_SHORT).show();
 
             setListAdapter(new ArrayAdapter<>(this,
                     android.R.layout.simple_list_item_1, fileList));
@@ -69,8 +75,10 @@ public class FileList extends ListActivity
 
     @Override
     public void onBackPressed() {
-        String parent = file.getParent().toString();
-        file = new File( parent ) ;
+
+        String parent = file.getParent();
+        file = new File(parent);
+
         File[] list = file.listFiles();
 
         fileList.clear();
@@ -78,10 +86,9 @@ public class FileList extends ListActivity
         for (File value : list) {
             fileList.add(value.getName());
         }
-        Toast.makeText(getApplicationContext(), parent, Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), parent, Toast.LENGTH_SHORT).show();
         setListAdapter(new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, fileList));
-
 
     }
 }
