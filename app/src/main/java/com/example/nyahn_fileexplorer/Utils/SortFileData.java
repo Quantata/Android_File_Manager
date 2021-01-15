7157758af5301c4f940ef4343267d7e1324bf9e3
Quantata <ceoahn95@gmail.com>
2021-01-15 15:38:42 +0900

    문자 + 숫자 파일 이름 정렬 개선

--- app/src/main/java/com/example/nyahn_fileexplorer/Utils/SortFileData.java
+++ app/src/main/java/com/example/nyahn_fileexplorer/Utils/SortFileData.java
@@ -12,13 +12,36 @@ public class SortFileData implements Comparator<FileData> {
 
     @Override
     public int compare(FileData o1, FileData o2) {
+
         String firstFileName = o1.getFile().getName();
         String secondFileName = o2.getFile().getName();
 
-        Log.d(TAG, "firstFileName = " + firstFileName);
-        Log.d(TAG, "secondFileName = " + secondFileName);
-        Log.d(TAG, "first compareTo second = " + firstFileName.compareTo(secondFileName));
+        if(extractString(firstFileName).compareTo(extractString(secondFileName)) == 0)
+            return extractInt(firstFileName) - extractInt(secondFileName);
+
 
         return firstFileName.compareTo(secondFileName);
+    }
+
+    private String extractString(String s) {
+        Log.d(TAG, "String s = " + s);
+
+        // 숫자가 아닌 것은 다 "" 만들어버림
+        String string = s.replaceAll("[0-9]", "");
+        Log.d(TAG, "String string = " + string);
+
+        // return "" if no characters found
+        return string.isEmpty() ? "" : string;
+    }
+
+    private int extractInt(String s) {
+        Log.d(TAG, "String s = " + s);
+
+        // 숫자가 아닌 것은 다 "" 만들어버림
+        String num = s.replaceAll("\\D", "");
+        Log.d(TAG, "String num = " + num);
+
+        // return 0 if no digits found
+        return num.isEmpty() ? 0 : Integer.parseInt(num);
     }
 }
